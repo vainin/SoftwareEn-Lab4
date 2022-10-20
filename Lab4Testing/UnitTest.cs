b2a7ebf419570c8016b9e8e025f4f4cec7390b16
vanin64 <vanin64@gmail.com>
2022-10-19 19:25:54 -0500

    Made a working version for update, delete, and add

--- Lab4Testing/UnitTest.cs
+++ Lab4Testing/UnitTest.cs
@@ -9,6 +9,12 @@ namespace Lab4Testing
         private string answer;
         private int difficulty;
         private string date;
+        private string cluel;
+        private string answerl;
+        private int difficultyl;
+        private string clueh;
+        private string answerh;
+        private int difficultyh;
         private int id;
 
         [SetUp]
@@ -18,14 +24,21 @@ namespace Lab4Testing
             clue = "";
             answer = "";
             difficulty = 1;
-            date = "";
+            date = "1/12/2022";
+            cluel = "";
+            answerl = "";
+            difficultyl = -1;
+            clueh = "I don't know how to make a clue way to long because it needs to be more than 250 characters so I will try and make it so. It still seems to be too short so another sentance will be added. " +
+                "250 characters is a lot for a clue and I hope to never see one that long.";
+            answerh = "This answer is way too long";
+            difficultyh = 4;
             id = bl.latestId;
         }
 
         [Test]
         public void TestAddNormal()
         {
-            bl.AddEntry(clue, answer, difficulty, date);
+            Assert.Equals(InvalidFieldError.NoError, bl.AddEntry(clue, answer, difficulty, date));
             id++;
             Assert.Equals(id, bl.latestId);
         }
@@ -33,23 +46,26 @@ namespace Lab4Testing
         [Test]
         public void TestAddHigh()
         {
-            bl.AddEntry(clue, answer, difficulty, date);
-            id++;
-            Assert.Equals(id, bl.latestId);
+
+            Assert.Equals(InvalidFieldError.InvalidClueLength, bl.AddEntry(clueh, answer, difficulty, date));
+            Assert.Equals(InvalidFieldError.InvalidAnswerLength, bl.AddEntry(clue, answerh, difficulty, date));
+            Assert.Equals(InvalidFieldError.InvalidDifficulty, bl.AddEntry(clue, answer, difficultyh, date));
+
         }
 
         [Test]
         public void TestAddLow()
         {
-            bl.AddEntry(clue, answer, difficulty, date);
-            id++;
-            Assert.Equals(id, bl.latestId);
+            Assert.Equals(InvalidFieldError.InvalidClueLength, bl.AddEntry(cluel, answerl, difficultyl, date));
+            Assert.Equals(InvalidFieldError.InvalidAnswerLength, bl.AddEntry(clue, answerl, difficulty, date));
+            Assert.Equals(InvalidFieldError.InvalidDifficulty, bl.AddEntry(clue, answer, difficultyl, date));
+
         }
 
         [Test]
         public void TestDeleteNormal()
         {
-            bl.DeleteEntry(id);
+            Assert.Equals(EntryDeletionError.NoError, bl.DeleteEntry(id));
             id--;
             Assert.Equals(id, bl.latestId);
         }
@@ -57,15 +73,15 @@ namespace Lab4Testing
         [Test]
         public void TestDeleteHigh()
         {
-            bl.DeleteEntry(id+1);
-            
+            Assert.Equals(EntryDeletionError.EntryNotFound, bl.DeleteEntry(id+1));
+
             Assert.Equals(id, bl.latestId);
         }
 
         [Test]
         public void TestDeleteLow()
         {
-            bl.DeleteEntry(-1);
+            Assert.Equals(EntryDeletionError.EntryNotFound, bl.DeleteEntry(-1));
             
             Assert.Equals(id, bl.latestId);
         }
@@ -74,8 +90,9 @@ namespace Lab4Testing
         public void TestUpdateNormal()
         {
             bl.EditEntry(clue, answer, difficulty, date, id);
-            
+            Entry entry = new Entry(clue, answer, difficulty, date, id);
             Assert.Equals(id, bl.latestId);
+            Assert.Equals(entry, bl.FindEntry(id));
         }
 
 
@@ -83,16 +100,18 @@ namespace Lab4Testing
         public void TestUpdateHigh()
         {
             bl.EditEntry(clue, answer, difficulty, date, id);
-            
+            Entry entry = new Entry(clue, answer, difficulty, date, id);
             Assert.Equals(id, bl.latestId);
+            Assert.Equals(entry, bl.FindEntry(id));
         }
 
         [Test]
         public void TestUpdateLow()
         {
             bl.EditEntry(clue, answer, difficulty, date, id);
-
+            Entry entry = new Entry(clue, answer, difficulty, date, id);
             Assert.Equals(id, bl.latestId);
+            Assert.Equals(entry, bl.FindEntry(id));
         }
 
 
